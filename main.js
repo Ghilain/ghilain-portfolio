5e1ee19613dcfef23e41f29326193a429dfd3841
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-04-12 17:25:20 +0200

    Add function.

--- main.js
+++ main.js
@@ -16,9 +16,10 @@ mobileMenu.addEventListener('click', () => {
 });
 //recent work
 const workSection = document.querySelector('#project');
+const cardList = document.getElementById('card')
 
 const projectsList = [
-  {
+  { id: 1,
     name: 'Profesional Art Printing Data',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
     featuredImage: './Images/Snapshoot-page-2.png',
@@ -28,6 +29,7 @@ const projectsList = [
   },
 
   {
+    id: 2,
     name: 'Profesional Art Printing Data',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
     featuredImage: './Images/Snapshoot-page-2.png',
@@ -36,6 +38,7 @@ const projectsList = [
     demoLink: 'https://christianib003.github.io/Portfolio-Moblie-Skeleton-Version/',
   },
   {
+    id: 3,
     name: 'Profesional Art Printing Data',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
     featuredImage: './Images/Snapshoot-page-2.png',
@@ -44,6 +47,7 @@ const projectsList = [
     demoLink: 'https://christianib003.github.io/Portfolio-Moblie-Skeleton-Version/',
   },
   {
+    id: 4,
     name: 'Profesional Art Printing Data',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
     featuredImage: './Images/Snapshoot-page-2.png',
@@ -52,6 +56,7 @@ const projectsList = [
     demoLink: 'https://christianib003.github.io/Portfolio-Moblie-Skeleton-Version/',
   },
   {
+    id: 5,
     name: 'Profesional Art Printing Data',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
     featuredImage: './Images/Snapshoot-page-2.png',
@@ -60,6 +65,7 @@ const projectsList = [
     demoLink: 'https://christianib003.github.io/Portfolio-Moblie-Skeleton-Version/',
   },
   {
+    id: 6,
     name: 'Profesional Art Printing Data',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
     featuredImage: './Images/Snapshoot-page-2.png',
@@ -68,3 +74,31 @@ const projectsList = [
     demoLink: 'https://christianib003.github.io/Portfolio-Moblie-Skeleton-Version/',
   },
 ];
+
+const projectCards = projectsList.map((cards,) => `
+<div class="cardwork1">
+<div class="container">
+  <div class="action">
+    <button class="buton1 buton5 butonpop" type="button"> See Project</button>
+  </div>
+  <div class="supporttext supporttext22">
+    <p class="supporttext1">
+    ${cards.description}
+    </p>
+  </div>
+  <h2 class="titlpost">
+    ${cards.name}
+  </h2>
+  <h2 class="titlpost2">
+    Website Protfolio 
+  </h2>
+  <ul class="tag">
+  ${cards.technologies.map((heading, index) => `<li class="info${index}">${heading}</li>`).join('')}
+ </ul>
+</div>              
+</div>
+
+`).join('');
+
+cardList.innerHTML += projectCards;
+

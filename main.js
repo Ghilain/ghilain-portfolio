58e283169882134da8c5b130298313bcde0a8e05
Ghilain <ishimweghilain5050@gmail.com>
2022-04-12 16:39:57 +0200

    Add object

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// mobile menu
 const openMenu = document.querySelector('.logo-text');
 const closeMenu = document.querySelector('.close-x');
 const mobileMenu = document.querySelector('.nav-list');
@@ -13,3 +14,19 @@ closeMenu.addEventListener('click', () => {
 mobileMenu.addEventListener('click', () => {
   mobileMenu.style.display = 'none';
 });
+//recent work
+const workSection = document.querySelector('#project');
+
+const projectsList = [
+  {
+    name: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    imageLink: './Images/Snapshoot-page-2.png',
+    technologies: ['html', 'css', 'javascript'],
+    role: 'Back end dev',
+    company: 'Canopy',
+    year: '2015',
+    sourceLink: 'https://github.com/Christianib003/Portfolio-Moblie-Skeleton-Version',
+    demoLink: 'https://christianib003.github.io/Portfolio-Moblie-Skeleton-Version/',
+  },
+];

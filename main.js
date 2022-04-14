bc03feb98e47d8f270de7d2f6299f33591304d91
Ghilain <ishimweghilain5050@gmail.com>
2022-04-14 23:48:16 +0200

    Add form validation

--- main.js
+++ main.js
@@ -331,3 +331,25 @@ function showPop(idi) {
     openinWindow.style.display = 'none';
   });
 }
+
+// form validation
+
+document.querySelector('#form').addEventListener('submit', (e) => {
+  let valid = true;
+
+  const responseContainer = document.querySelector('#warning-message');
+
+  const emailArray = document.querySelector('#email').value.split('');
+
+  for (let i = 0; i < emailArray.length; i += 1) {
+    if (
+      emailArray[i] !== emailArray[i].toLowerCase() && emailArray[i] !== '@' && emailArray[i] !== '.') {
+      valid = false;
+    }
+  }
+
+  if (!valid) {
+    e.preventDefault();
+    responseContainer.innerHTML = 'Invalid Email: Email contain capital letter';
+  }
+});

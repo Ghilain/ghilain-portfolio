fd16ea0bd31ecf5ba324ecf3c080af537c6b387c
Ghilain <ishimweghilain5050@gmail.com>
2022-10-26 12:34:07 +0200

    Add new project cards

--- main.js
+++ main.js
@@ -20,9 +20,9 @@ const cardList = document.getElementById('pop');
 const projectsList = [
   {
     id: 1,
-    name: 'Profesional Art Printing Data',
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    featuredImage: './Images/Snapshoot-page-2.png',
+    name: 'Kwita Izina Event',
+    description: 'The Kwita Izina gorilla naming ceremony is modelled off a centuries-old tradition in Rwanda.',
+    featuredImage: './images/kwita-izina.PNG',
     technologies: 'html',
     technologies1: 'css',
     technologies2: 'javascript',
@@ -35,8 +35,8 @@ const projectsList = [
 
   {
     id: 2,
-    name: 'Profesional Art Printing Data',
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    name: 'Food Recipe',
+    description: 'This is a Capstone project being built in the second module of curriculum at microverse.',
     featuredImage: './Images/Snapshoot-page-2.png',
     technologies: 'html',
     technologies1: 'css',
@@ -49,10 +49,10 @@ const projectsList = [
   },
   {
     id: 3,
-    name: 'Profesional Art Printing Data',
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    name: 'Covid data',
+    description: 'Web app showing COVID data for the country have more cases around world and use disease.sh api data',
     featuredImage: './Images/Snapshoot-page-2.png',
-    technologies: 'html',
+    technologies: 'react',
     technologies1: 'css',
     technologies2: 'javascript',
     sourceLink: 'https://ghilain.github.io/mobile-version-skeleton/',
@@ -63,11 +63,11 @@ const projectsList = [
   },
   {
     id: 4,
-    name: 'Profesional Art Printing Data',
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    name: 'Budget App',
+    description: 'The Ruby on Rails capstone project is about building a mobile web application where you can manage your budget.',
     featuredImage: './Images/Snapshoot-page-2.png',
-    technologies: 'html',
-    technologies1: 'css',
+    technologies: 'ruby',
+    technologies1: 'ruby on rails',
     technologies2: 'javascript',
     sourceLink: 'https://ghilain.github.io/mobile-version-skeleton/',
     demoLink: 'https://github.com/Ghilain/mobile-version-skeleton/',
@@ -77,11 +77,11 @@ const projectsList = [
   },
   {
     id: 5,
-    name: 'Profesional Art Printing Data',
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    name: 'Recipe Food',
+    description: 'The Recipe food app keeps track of all your recipes, and ingredients.',
     featuredImage: './Images/Snapshoot-page-2.png',
-    technologies: 'html',
-    technologies1: 'css',
+    technologies: 'ruby',
+    technologies1: 'ruby on rails',
     technologies2: 'javascript',
     sourceLink: 'https://ghilain.github.io/mobile-version-skeleton/',
     demoLink: 'https://github.com/Ghilain/mobile-version-skeleton/',
@@ -91,11 +91,11 @@ const projectsList = [
   },
   {
     id: 6,
-    name: 'Profesional Art Printing Data',
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    name: 'Motorbike Booking',
+    description: 'Motorbike Booking is a react frontend application for booking Motorbikes.',
     featuredImage: './Images/Snapshoot-page-2.png',
-    technologies: 'html',
-    technologies1: 'css',
+    technologies: 'react',
+    technologies1: 'ruby on rails',
     technologies2: 'javascript',
     sourceLink: 'https://ghilain.github.io/mobile-version-skeleton/',
     demoLink: 'https://github.com/Ghilain/mobile-version-skeleton/',
@@ -107,7 +107,7 @@ const projectsList = [
 
 const projectCards = projectsList.map((cards) => `
 <li class="size bg${cards.id}">
-          <div class="temp2 ${cards.spec}">
+          <div class="temp${cards.id} ${cards.spec}">
             <div class="pad">
               <h3 class="work card-heading">${cards.name}</h3>
               <p class="text text-2">
@@ -121,7 +121,7 @@ const projectCards = projectsList.map((cards) => `
             </div>
           </div>
           <div>
-            <button id="btncli${cards.id}" onclick='showPop(${cards.id})' type="submit" class="btn btns btn-2 ${cards.butt}">see project</button>
+            <button id="btncli${cards.id}" onclick='showPop(${cards.id})' type="submit" class="btn btns btn-2">see project</button>
           </div>
         </li>
 
@@ -136,17 +136,13 @@ const box = document.getElementById('popupwindow');
 const popWindowContent = [
   {
     idi: 0,
-    name1: 'Multi Post Stories',
-    name2: 'Keeping track of hundreds  of components website',
+    name1: 'Space Travelers Hub',
+    name2: 'Space Travelers Hub',
     closee: '&times;',
-    tech: ['html', 'bootstrap', 'Ruby on rails'],
-    featuredImagepop: './images/popupdesk.png',
-    popDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting
-  industry. Lorem Ipsum has been the industry's standard dummy text
-  ever since the 1500s, when an unknown printer took a galley of
-  type and scrambled it to make a type specimen book. It has
-  survived not only five centuries, but also the leap into
-  electronic typesetting, remaining essent`,
+    tech: ['react', 'javascript', 'redux'],
+    featuredImagepop: './images/space-traveler-hub.PNG',
+    popDescription: `Space Travelers Hub is web application for a company that provides commercial and scientific space travel services. Consists of Rockets, Missions, and the My Profile section.
+    In this application users can book rockets and join selected space missions available to go in the space.`,
     imageSeelive: 'images/seelive.png',
     imageSeeproject: 'images/logogit.png',
     imagSeen: 'img-popup',
@@ -155,18 +151,18 @@ const popWindowContent = [
     liveSource: 'live-source',
     ulistPop: 'puOut',
     livePrg: 'live-prg',
+    liveslink: 'https://space-traveler-hubs.herokuapp.com/',
+    sourcelink: 'https://github.com/Ghilain/space-travelers-hub',
 
   },
   {
     idi: 1,
-    name1: 'Profesional Art Printing Data',
-    name2: 'Keeping track of hundreds  of components website',
+    name1: 'Kwita Izina Event',
+    name2: 'Kwita Izina Event',
     closee: '&times;',
-    tech: ['html', 'bootstrap', 'Ruby on rails'],
-    featuredImagepop: './images/MaskGroup3.png',
-    popDescription: `A daily selection of privately personalized
-     reads; no accounts or sign-ups required. has been the 
-     industry's standard`,
+    tech: ['html', 'css', 'javascript'],
+    featuredImagepop: './images/kwita-izina.PNG',
+    popDescription: 'The Kwita Izina gorilla naming ceremony is modelled off a centuries-old tradition in which Rwandans name their children in the presence of family and friends. And happen every year.',
     imageSeelive: 'images/seelive.png',
     imageSeeproject: 'images/logogit.png',
     imagSeen: 'img-popup1',
@@ -175,17 +171,18 @@ const popWindowContent = [
     liveSource: 'live-source1',
     ulistPop: 'puOut1',
     livePrg: 'live-prg1',
+    liveslink: 'https://ghilain.github.io/kwita-izina-event/',
+    sourcelink: 'https://github.com/Ghilain/kwita-izina-event',
+
   },
   {
     idi: 2,
-    name1: 'Profesional Art Printing Data',
-    name2: 'Keeping track of hundreds  of components website',
+    name1: 'Food Recipe',
+    name2: 'Food Recipe',
     closee: '&times;',
-    tech: ['html', 'bootstrap', 'Ruby on rails'],
-    featuredImagepop: './images/MaskGroup3.png',
-    popDescription: `A daily selection of privately personalized
-     reads; no accounts or sign-ups required. has been the 
-     industry's standard`,
+    tech: ['html', 'css', 'javascript'],
+    featuredImagepop: './images/food-recipee.PNG',
+    popDescription: "This is a Capstone project being built in the second module of curriculum at microverse. it's an application that uses the MealDB API to display meals and instructions on how to make the meals. The project also utilizes the Involvement API to update and display comments and likes for the meals",
     imageSeelive: 'images/seelive.png',
     imageSeeproject: 'images/logogit.png',
     imagSeen: 'img-popup1',
@@ -194,18 +191,18 @@ const popWindowContent = [
     liveSource: 'live-source1',
     ulistPop: 'puOut1',
     livePrg: 'live-prg1',
+    liveslink: 'https://ghilain.github.io/food-recipee/dist/index.html',
+    sourcelink: 'https://github.com/Ghilain/food-recipee',
 
   },
   {
     idi: 3,
-    name1: 'Profesional Art Printing Data',
-    name2: 'Keeping track of hundreds  of components website',
+    name1: 'Covid data',
+    name2: 'Covid data',
     closee: '&times;',
-    tech: ['html', 'bootstrap', 'Ruby on rails'],
-    featuredImagepop: './images/MaskGroup3.png',
-    popDescription: `A daily selection of privately personalized
-     reads; no accounts or sign-ups required. has been the 
-     industry's standard`,
+    tech: ['react', 'css', 'javascript'],
+    featuredImagepop: './images/covid-pro.PNG',
+    popDescription: 'Web app showing COVID data for the country have more cases around world and use disease.sh api data',
     imageSeelive: 'images/seelive.png',
     imageSeeproject: 'images/logogit.png',
     imagSeen: 'img-popup1',
@@ -214,18 +211,18 @@ const popWindowContent = [
     liveSource: 'live-source1',
     ulistPop: 'puOut1',
     livePrg: 'live-prg1',
+    liveslink: 'https://frabjous-crisp-b21f74.netlify.app/',
+    sourcelink: 'https://github.com/Ghilain/covid-data',
 
   },
   {
     idi: 4,
-    name1: 'Profesional Art Printing Data',
-    name2: 'Keeping track of hundreds  of components website',
+    name1: 'Budget App',
+    name2: 'Budget App',
     closee: '&times;',
-    tech: ['html', 'bootstrap', 'Ruby on rails'],
-    featuredImagepop: './images/MaskGroup3.png',
-    popDescription: `A daily selection of privately personalized
-     reads; no accounts or sign-ups required. has been the 
-     industry's standard`,
+    tech: ['ruby', 'bootstrap', 'Ruby on rails'],
+    featuredImagepop: './images/budget-app.PNG',
+    popDescription: 'The Ruby on Rails capstone project is about building a mobile web application where you can manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what.',
     imageSeelive: 'images/seelive.png',
     imageSeeproject: 'images/logogit.png',
     imagSeen: 'img-popup1',
@@ -234,18 +231,18 @@ const popWindowContent = [
     liveSource: 'live-source1',
     ulistPop: 'puOut1',
     livePrg: 'live-prg1',
+    liveslink: 'https://desolate-fjord-22181.herokuapp.com/',
+    sourcelink: 'https://github.com/Ghilain/budget-app/tree/dev',
 
   },
   {
     idi: 5,
-    name1: 'Profesional Art Printing Data',
-    name2: 'Keeping track of hundreds  of components website',
+    name1: 'Recipe Food',
+    name2: 'Recipe Food',
     closee: '&times;',
-    tech: ['html', 'bootstrap', 'Ruby on rails'],
-    featuredImagepop: './images/MaskGroup3.png',
-    popDescription: `A daily selection of privately personalized
-     reads; no accounts or sign-ups required. has been the 
-     industry's standard`,
+    tech: ['ruby', 'css3', 'Ruby on rails'],
+    featuredImagepop: './images/food-recipee.PNG',
+    popDescription: 'The Recipe food app keeps track of all your recipes, and ingredients. It will allow you to save ingredients, keep track of what you have, create recipes, and generate a shopping list based on what you have and what you are missing from a recipe. Also, since sharing recipes is an important part of cooking the app should allow you to make them public so anyone can access them.',
     imageSeelive: 'images/seelive.png',
     imageSeeproject: 'images/logogit.png',
     imagSeen: 'img-popup1',
@@ -254,18 +251,18 @@ const popWindowContent = [
     liveSource: 'live-source1',
     ulistPop: 'puOut1',
     livePrg: 'live-prg1',
+    liveslink: 'https://github.com/Ghilain/recipes-food',
+    sourcelink: 'https://github.com/Ghilain/recipes-food',
 
   },
   {
     idi: 6,
-    name1: 'Profesional Art Printing Data',
-    name2: 'Keeping track of hundreds  of components website',
+    name1: 'Motorbike Booking',
+    name2: 'Motorbike Booking',
     closee: '&times;',
-    tech: ['html', 'bootstrap', 'Ruby on rails'],
-    featuredImagepop: './images/MaskGroup3.png',
-    popDescription: `A daily selection of privately personalized
-     reads; no accounts or sign-ups required. has been the 
-     industry's standard`,
+    tech: ['ruby', 'react', 'Ruby on rails'],
+    featuredImagepop: './images/motobikin-phone.PNG',
+    popDescription: 'Motorbike Booking is a react frontend application for booking Motorbikes. This application serves as user interface for the backend and users can create, and delete Motorbikes, as well as add reservations. Users can sign up or sign in to able to book motor.',
     imageSeelive: 'images/seelive.png',
     imageSeeproject: 'images/logogit.png',
     imagSeen: 'img-popup1',
@@ -274,6 +271,8 @@ const popWindowContent = [
     liveSource: 'live-source1',
     ulistPop: 'puOut1',
     livePrg: 'live-prg1',
+    liveslink: 'https://motorbike-booking.netlify.app/',
+    sourcelink: 'https://github.com/RWUBAKWANAYO/motorbike_booking_frontend',
 
   },
 ];
@@ -299,10 +298,10 @@ function popData(popwindow) {
 ${popwindow.popDescription}
 </p>
 <div class="${popwindow.liveSource}">
-<button class="${popwindow.buttonPop}" type="button">
+<button class="${popwindow.buttonPop}" onclick=" window.open('${popwindow.liveslink}','_blank')" type="button">
  See Live <img src="${popwindow.imageSeelive}" alt="live icon" />
 </button>
-<button class="${popwindow.buttonPop1}" type="button">
+<button class="${popwindow.buttonPop1}" onclick=" window.open('${popwindow.sourcelink}','_blank')" type="button">
  See Source <img src="${popwindow.imageSeeproject}" alt="github-icon" />
 </button>
 </div>

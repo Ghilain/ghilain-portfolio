8c559cbbba3fc94859e86a443292e47b9209a866
Ghilain <ishimweghilain5050@gmail.com>
2023-01-25 22:18:26 +0200

    updates

--- main.js
+++ main.js
@@ -241,7 +241,7 @@ const popWindowContent = [
     name2: 'Recipe Food',
     closee: '&times;',
     tech: ['ruby', 'css3', 'Ruby on rails'],
-    featuredImagepop: './images/food-recipee.PNG',
+    featuredImagepop: './images/recipe-food.PNG',
     popDescription: 'The Recipe food app keeps track of all your recipes, and ingredients. It will allow you to save ingredients, keep track of what you have, create recipes, and generate a shopping list based on what you have and what you are missing from a recipe. Also, since sharing recipes is an important part of cooking the app should allow you to make them public so anyone can access them.',
     imageSeelive: 'images/seelive.png',
     imageSeeproject: 'images/logogit.png',
